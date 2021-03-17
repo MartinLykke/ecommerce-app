f6f27488df1c774f4d8a95e22eb0b296f3edaaf2
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2021-03-17 19:48:19 +0100

    Navbar updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { connect } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Signup from "./components/pages/Signup";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Cart from "./components/pages/Cart";
+import Login from "./components/pages/Login";
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path="/" exact component={Home} />
             <Route path="/signup" exact component={Signup} />
             <Route path="/cart" exact component={Cart} />
+            <Route path="/login" exact component={Login} />
           </Switch>
         </Router>
       </>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as ReactBootStrap from "react-bootstrap";
-import { Cart2, Search } from "react-bootstrap-icons";
+import { Cart2, Search, Person, Circle } from "react-bootstrap-icons";
+import "./Navbar.css";
 function Navbar() {
   return (
     <div>
@@ -19,36 +20,47 @@ function Navbar() {
                   <Search></Search>
                 </ReactBootStrap.Button>
               </ReactBootStrap.Form>
-              <ReactBootStrap.NavDropdown title="Men" id="basic-nav-dropdown">
+              <ReactBootStrap.NavDropdown
+                title="All products"
+                id="basic-nav-dropdown"
+              >
                 <ReactBootStrap.NavDropdown.Item href="#action/3.1">
-                  Tøj
+                  Hardware
                 </ReactBootStrap.NavDropdown.Item>
                 <ReactBootStrap.NavDropdown.Item href="#action/3.2">
-                  Sko
+                  Pc & tablets
                 </ReactBootStrap.NavDropdown.Item>
-              </ReactBootStrap.NavDropdown>
-              <ReactBootStrap.NavDropdown title="Women" id="basic-nav-dropdown">
-                <ReactBootStrap.NavDropdown.Item href="#action/3.1">
-                  Tøj
-                </ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item href="#action/3.2">
-                  Sko
+                <ReactBootStrap.NavDropdown.Item href="#action/3.3">
+                  Mobil
                 </ReactBootStrap.NavDropdown.Item>
               </ReactBootStrap.NavDropdown>
             </ReactBootStrap.Nav>
           </ReactBootStrap.Navbar.Collapse>
           <ReactBootStrap.Navbar.Brand
-            className="position-absolute top-25 start-50"
+            className="position-absolute top-25 start-50 logo"
             href="/"
           >
             Logo
           </ReactBootStrap.Navbar.Brand>
-          <h2 className="absolute sticky-top">
-            <ReactBootStrap.Nav.Link href="/cart">
-              {" "}
-              <Cart2 />
-            </ReactBootStrap.Nav.Link>{" "}
-          </h2>
+
+          <div className="navbar-menu ">
+            <a className="icon-button" href="/login">
+              <div className="icon-container text-dark">
+                <h3>
+                  <Person />
+                </h3>
+              </div>
+            </a>
+            <a className="icon-button" href="/cart">
+              <div className="icon-container text-dark">
+                <h3>
+                  <Cart2 />
+                </h3>
+                <span className="icon-price">799</span>
+                <span className="icon-number">1</span>
+              </div>
+            </a>
+          </div>
         </ReactBootStrap.Container>
       </ReactBootStrap.Navbar>
     </div>
